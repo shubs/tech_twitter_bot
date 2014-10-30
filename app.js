7dbb1fd5a6802ccdf0610eeedb06f4f1bb6597f1
Shubham SHARMA <shubham@sharma.fr>
2014-10-30 23:32:10 +0100

    first one

---
+++ app.js
@@ -0,0 +1,15 @@
+// This file have to be created in the root folder to specify your twitter and mailjet credentials
+var credentials = require('./credentials.js');
+
+var Twit = require('twit');
+var T = new Twit({
+		consumer_key: 'BsbTj5MCLeTaUFATZrRlVmmSj',
+		consumer_secret: 'ygf8eXWlQrWLigkOJAG7lFc1QkgTGU7Wwoh4ZJxYABrSprLyl1',
+		access_token: '39555082-JIlybB8E7KOG5RIXfSrEmpmIzV417lDK8CdTqvBw1',
+		access_token_secret: 'TIFGj8ZeZQLxFikdmAxtOuSV5uptYCHjtf8hkCHxwkK6b'
+});
+//
+//  tweet 'hello world!'
+//
+
+
