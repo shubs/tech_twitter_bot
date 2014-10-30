f332ea019dfc03ae0c336ed24c0733ca92a9cd49
Shubham SHARMA <shubham@sharma.fr>
2014-10-30 23:46:14 +0100

    WIP

--- app.js
+++ app.js
@@ -2,14 +2,74 @@
 var credentials = require('./credentials.js');
 
 var Twit = require('twit');
-var T = new Twit({
-		consumer_key: 'BsbTj5MCLeTaUFATZrRlVmmSj',
-		consumer_secret: 'ygf8eXWlQrWLigkOJAG7lFc1QkgTGU7Wwoh4ZJxYABrSprLyl1',
-		access_token: '39555082-JIlybB8E7KOG5RIXfSrEmpmIzV417lDK8CdTqvBw1',
-		access_token_secret: 'TIFGj8ZeZQLxFikdmAxtOuSV5uptYCHjtf8hkCHxwkK6b'
-});
+
 //
 //  tweet 'hello world!'
 //
+var Bot = module.exports = function() { 
+  this.twit = new Twit(credentials.twitter);
+};
 
 
+
+//
+//  choose a random friend of one of your followers, and follow that user
+//
+Bot.prototype.mingle = function (callback) {
+  var self = this;
+  
+  this.twit.get('followers/ids', function(err, reply) {
+      if(err) { return callback(err); }
+      
+      var followers = reply.ids
+        , randFollower  = randIndex(followers);
+        
+      self.twit.get('friends/ids', { user_id: randFollower }, function(err, reply) {
+          if(err) { return callback(err); }
+          
+          var friends = reply.ids
+            , target  = randIndex(friends);
+            
+          self.twit.post('friendships/create', { id: target }, callback);
+        })
+    })
+};
+
+//
+//  prune your followers list; unfollow a friend that hasn't followed you back
+//
+Bot.prototype.prune = function (callback) {
+  var self = this;
+  
+  this.twit.get('followers/ids', function(err, reply) {
+      if(err) return callback(err);
+      
+      var followers = reply.ids;
+      
+      self.twit.get('friends/ids', function(err, reply) {
+          if(err) return callback(err);
+          
+          var friends = reply.ids
+            , pruned = false;
+          
+          while(!pruned) {
+            var target = randIndex(friends);
+            
+            if(!~followers.indexOf(target)) {
+              pruned = true;
+              self.twit.post('friendships/destroy', { id: target }, callback);   
+            }
+          }
+      });
+  });
+};
+
+function randIndex (arr) {
+  var index = Math.floor(arr.length*Math.random());
+  return arr[index];
+};
+
+setInterval(function() {
+	B = new Bot();
+	B.mingle(console.log);
+}, 1000);
