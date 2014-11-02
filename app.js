48d0004e3da4109122c1f9af3917c21b0baf1014
Shubham SHARMA <shubham@sharma.fr>
2014-11-02 02:38:43 +0100

    strat begin

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ function randIndex (arr) {
 	return arr[index];
 }
 
+function follow(target){
+	T.post('friendships/create', {user_id: target, follow: true},function (err, data, response) {
+		if(err) { console.log(err); }
+		console.log("followed");
+	});
+}
+
+function good_friend_to_follow(id){
+
+}
+
 function go(){
 	T.get('followers/ids',	function (err, data, response) {
 			if(err) { console.log(err); }
@@ -23,17 +34,22 @@ function go(){
 			if(err) { console.log(err); }
 
 			var friends = data.ids;
-			var target	= randIndex(friends);
-			console.log(target);
 
-			T.post('friendships/create', {user_id: target, follow: true},function (err, data, response) {console.log(err);});
-
+			setInterval(function() {
+				var target	= randIndex(friends);
+				console.log(target);
+				T.get('users/lookup', { user_id: target },  function (err, data, response) {
+					if(err) { console.log(err); }
+					console.log(data[0].screen_name + " followers : "+data[0].followers_count + ", followings : "+data[0].friends_count);
+					var good = ((data[0].followers_count < 700) && (data[0].friends_count > 200));
+					if (good){
+						follow(target);
+					}
+				});
+			}, 5000);
 		});
 
 	});
 }
 
-
-setInterval(function() {
-	go();
-}, 5000);
+go();
